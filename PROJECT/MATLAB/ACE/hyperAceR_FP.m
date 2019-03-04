b4441a73969628ad9fefb48c7b49eb4e7aff4ad7
Dordije <dordijeb@stud.ntnu.no>
2019-03-04 10:47:30 +0100

    AceR FP
    works

--- PROJECT/MATLAB/ACE/hyperAceR_FP.m
+++ PROJECT/MATLAB/ACE/hyperAceR_FP.m
@@ -11,26 +11,114 @@ function [results] = hyperAceR_FP(M, S)
 % Outputs
 %   results - vector of detector output (N x 1)
 
-	[p, N] = size(M);
+outsize = 32;
+insize = 16;
+%T1 = numerictype(1,outsize,31);
+[p, N] = size(M);
 
-	R = hyperCorr(M);
-	G = inv(R);
+if (max(M(:)) > 2^15)
+    M = M / 2;
+end
 
- %start Fixed point conversion from here
- 
-    G = int32(G*2^16); 
-    S = int32(S*2^16);
-    G = double (G);
-    S = double (S);
-	results = zeros(1, N);
-	
-	tmp = (S.'*G*S);
-	
-	for k=1:N
-		
-		x = M(:,k);
-		results(k) = (S.'*G*x)^2 / (tmp*(x.'*G*x));
-		
-	end
+M = floor(M);
+
+add_frac = ceil(log2(p));
+R = hyperCorr(M);
+G = inv(R);
+
+%start Fixed point conversion from here
+
+%Gfp = sfi(G,32);
+Gfp = floor(G*2^39);
+
+sG = S' * G;
+%sGfp = sfi(sG,32);
+sGfp = floor(sG*2^39);
+
+tmp = (S.' * G * S);
+%tmpfp= sfi(tmp,32);
+tmpfp = floor(tmp*2^28);
+
+results = zeros(1, N);
+
+%number of oveflows
+cnt1 = 0; 
+cnt2 = 0;  
+cnt3 = 0;  
+cnt4 = 0;  
+cnt5 = 0;
+
+%division type
+T = numerictype('Signed', true, 'WordLength', 32, ...
+    'FractionLength', 30);
+
+a = sfi(1, 2, 0);
+
+for k = 1:N
+    
+    x = M(:, k);
+    
+    t1 = x' * Gfp;
+   % t1 = floor(t1./2^(insize-1+add_frac));
+    t1 = floor(t1);
+    if (t1(:) > 2^31 - 1)
+        t1(t1>2^31-1)=2^31-1;
+        cnt1 = cnt1 + 1;
+    elseif (t1(:) < -2^31)
+        t1(t1<-2^31)=-2^31;
+        cnt1 = cnt1 + 1;
+    end
+    
+    t2 = t1 * x;
+    t2 = floor(t2./2^(insize-1+add_frac));
+    if (t2 > 2^31 - 1)
+        t2 = 2^31 - 1;
+        cnt2 = cnt2 + 1;
+    elseif (t2 < -2^31)
+        t2 = -2^31;
+        cnt2 = cnt2 + 1;
+    end
+    
+    
+    sGx = sGfp * x;
+    sGx = floor(sGx./2^(insize-1+add_frac));
+    if (sGx > 2^31 - 1)
+        sGx = 2^31 - 1;
+        cnt3 = cnt3 + 1;
+    elseif (sGx < -2^31)
+        sGx = -2^31;
+        cnt3 = cnt3 + 1;
+    end
+    
+    
+    sGx2 = sGx * sGx;
+    sGx2 = floor(sGx2./2^(outsize-1));
+    if (sGx2 > 2^31 - 1)
+        sGx2 = 2^31 - 1;
+        cnt4 = cnt4 + 1;
+    elseif (sGx2 < -2^31)
+        sGx2 = -2^31;
+        cnt4 = cnt4 + 1;
+    end
+    
+    
+    d2 = tmpfp * t2;
+    d2 = floor(d2./2^(outsize-1));
+    if (d2 > 2^31 - 1)
+        d2 = 2^31 - 1;
+        cnt5 = cnt5 + 1;
+    elseif (d2 < -2^31)
+        d2 = -2^31;
+        cnt5 = cnt5 + 1;
+    end
+    
+%     d2 = sfi(d2, 32, 0);
+%     sGx2 = sfi(sGx2, 32, 0);
+%     c = divide(T, a, d2);
+%   
+%     results(k) = double(sGx2*c);
+      results(k) = sGx2/d2;
+    
+end
 
 end
