eb80b97865c32253efe6f5ddb03ff165f4dfec13
Dordije <dordijeb@stud.ntnu.no>
2019-04-01 11:52:17 +0200

    ENVI files loading

---
+++ MATLAB/Dordije_Code/loadENVI.m
@@ -0,0 +1,23 @@
+clc;
+
+%% initial 
+name = 'self_test_refl';
+name = 'F1';
+img_or_spl = 1;
+
+%% loading
+namehdr=[name,'.hdr'];
+
+if(img_or_spl == 1)
+    nameimg=[name,'.img'];
+else
+    nameimg=[name,'.spl'];
+end    
+info=read_envihdr(namehdr);
+
+X=multibandread(nameimg,info.size,info.format,info.header_offset,...
+    info.interleave,info.machine);
+
+
+%% clearing
+%clearvars -except X 
